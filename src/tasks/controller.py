b0e18ceaf6e526ba2499aa1b99987f4942fbe500
JishnudipSaha <jishnudipsaha.10a.15.02062004@gmail.com>
2026-09-13 15:32:45 +0530

    updated the router service for create task

--- src/tasks/controller.py
+++ src/tasks/controller.py
@@ -13,10 +13,7 @@ def create_task(body: TaskSchema, db: Session):
     db.commit()
     db.refresh(new_task)
     
-    return {
-        "status": "Task Created Successfully.......",
-        "data": new_task
-    }
+    return new_task
     
     
 def get_tasks(db: Session):
