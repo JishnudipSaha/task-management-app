60bac1ffc5de2600d7dd61c49ce2b31ded2e6e1d
JishnudipSaha <jishnudipsaha.10a.15.02062004@gmail.com>
2026-09-16 16:20:34 +0530

    initiated the implmentation for users

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from src.utils.db import Base, engine
 from src.tasks.models import TaskModel
 from src.tasks.router import task_routes
+from src.user.router import user_routes
 
 
 Base.metadata.create_all(engine)
@@ -10,6 +11,7 @@ Base.metadata.create_all(engine)
 
 app = FastAPI(title='This is a task management application')
 app.include_router(task_routes)
+app.include_router(user_routes)
 
 
 
---
+++ src/user/router.py
@@ -0,0 +1,15 @@
+from this import d
+from fastapi import APIRouter, Depends, status
+from starlette.status import HTTP_100_CONTINUE, HTTP_201_CREATED
+from src.user.dtos import UserSchema
+from sqlalchemy.orm import Session
+from src.utils.db import get_db
+from src.user import controller
+
+
+user_routes = APIRouter(prefix='/user')
+
+
+@user_routes.post('/register', status_code=HTTP_201_CREATED)
+def register(body: UserSchema, db: Session = Depends(get_db)):
+    return controller.register(body = body, db = db)
